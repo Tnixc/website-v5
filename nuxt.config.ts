905d82571df8b9841ab0b119c5c62c8c6275a450
Tnixc <tnixxc@gmail.com>
2024-02-25 17:38:02 -0500

    Add Nuxt modules and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
+import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "@nuxt/image",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/seo",
+    "@nuxtjs/partytown",
+    "shadcn-nuxt"
+  ],
+  extends: [
+    "nuxt-umami"
+  ],
+  alias: {
+    '@': ".",
+  },
+  css: []
+});

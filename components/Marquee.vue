ddb4d73566e379396c6c9579dcb42f7f62a61f1b
Tnixc <tnixxc@gmail.com>
2024-02-27 16:37:12 -0500

    marquee gradient text

--- components/Marquee.vue
+++ components/Marquee.vue
@@ -1,6 +1,9 @@
 <template>
-  <Vue3Marquee duration="60" class="py-10">
-    <template v-for="item in marqueeItems" :key="item">
+  <div
+    class="aurora-overlay z-10 absolute top-32 h-[200px] left-0 w-full mix-blend-lighten dark:mix-blend-darken bg-gradient-to-r from-violet-300 via-cyan-300 to-amber-300">
+  </div>
+  <Vue3Marquee duration="60" class="py-10 relative overflow-clip ">
+    <template v-for="item in marqueeItems" :key="item" class="mix-blend-hard-light">
       <span class="text-9xl px-3">{{ item }} ー </span>
     </template>
   </Vue3Marquee>
@@ -9,7 +12,7 @@
 import { marqueeItems } from '@/data/marquee'
 </script>
 <style scoped>
-  span{
-    font-family: "Satoshi"
-  }
+span {
+  font-family: "Satoshi"
+}
 </style>

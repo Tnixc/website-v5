44c5179428789846e628e07988d7b18be257f471
Tnixc <tnixxc@gmail.com>
2024-03-01 22:26:02 -0500

    switch theme to everforest

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,7 +5,7 @@
       class="relative text-2xl duration-1000 before:absolute before:inset-x-full before:top-full before:h-0.5 before:origin-right before:bg-primary before:transition-all hover:before:inset-x-0 hover:before:top-full"
       exact-active-class="active" to="/">Tnixc</NuxtLink>
     <div class="flex-grow"></div>
-    <div 
+    <div v-if="menu"
       class="absolute top-20 right-0 z-50 flex flex-col gap-10 p-5 md:static md:flex-row md:gap-5 md:p-0 bg-background">
       <NuxtLink
         class="relative text-2xl duration-1000 before:absolute before:inset-x-full before:top-full before:h-0.5 before:origin-right before:bg-primary before:transition-all hover:before:inset-x-0 hover:before:top-full"

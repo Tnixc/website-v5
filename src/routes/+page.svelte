52c9dd4e6d0cd3f98e97d6bce04179eb5207110d
Tnixc <tnixxc@gmail.com>
2024-04-14 11:41:17 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-blue-500">Hello world from main</h1>
+<a href="/other"> to Other</a>
---
+++ src/routes/other/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-blue-500">Hello world from other</h1>
+<a href="/">to main</a>
+

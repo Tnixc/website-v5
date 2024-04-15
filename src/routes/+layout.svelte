765bc7f9e6572fcb9bbad2a8479171bda0f461e3
Tnixc <tnixxc@gmail.com>
2024-04-14 22:03:45 -0400

    more fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 import "../app.css";
+import "../fonts.css";
 import { Toaster } from "svelte-sonner";
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		fontFamily: {
-			sans: ["zed-sans-extended", "ui-sans-serif", "system-ui"],
-			bold: ["zed-sans-extendedbold", "ui-sans-serif", "system-ui"],
+			zed: ["zed-sans-extended", "ui-sans-serif", "system-ui"],
+			"zed-bold": ["zed-sans-extendedbold", "ui-sans-serif", "system-ui"],
+			general: ["GeneralSans-Regular", "ui-sans-serif", "system-ui"],
+			"general-italic": ["GeneralSans-Italic", "ui-sans-serif", "system-ui"],
+			"general-bold": ["GeneralSans-Bold", "ui-sans-serif", "system-ui"],
+			"general-bold-italic": [
+				"GeneralSans-BoldItalic",
+				"ui-sans-serif",
+				"system-ui",
+			],
 			display: ["Zodiak-LightItalic", "ui-serif", "Georgia"],
 		},
 		extend: {},

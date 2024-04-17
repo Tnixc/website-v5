6507e3731d4b0b30b38fdaa4c51ebc4a42af1799
Tnixc <tnixxc@gmail.com>
2024-04-17 13:51:43 -0400

    working

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,37 +1,10 @@
 <script lang="ts">
 import openicon from "pixelarticons/svg/open.svg";
 import WindowContent from "./WindowContent.svelte";
-let windowOpen = false;
-async function sleep(ms: number) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
-async function close() {
-	if (!windowOpen) {
-		return;
-	}
-	windowOpen = false;
-	const button = document.getElementById("projects")!;
-	const buttonW = button.getBoundingClientRect().width;
-	const buttonH = button.getBoundingClientRect().height;
-	const buttonX = button.getBoundingClientRect().x;
-	const buttonY = button.getBoundingClientRect().y;
-	const window = document.getElementById("window")!;
-	const windowLayer = document.querySelector(".window-layer")!;
-	window.style.transform = "";
-	window.style.width = `${buttonW}px`;
-	window.style.height = `${buttonH}px`;
-	window.style.left = `${buttonX}px`;
-	window.style.top = `${buttonY}px`;
-	await sleep(200);
-	window.remove();
-	windowLayer.classList.add("opacity-0", "pointer-events-none");
-}
+import { sleep } from "../utils";
 async function expand() {
-	if (windowOpen) {
-		return;
-	}
-	windowOpen = true;
 	const button = document.getElementById("projects")!;
+	button.style.visibility = "hidden";
 	const buttonW = button.getBoundingClientRect().width;
 	const buttonH = button.getBoundingClientRect().height;
 	const buttonX = button.getBoundingClientRect().x;
@@ -42,11 +15,8 @@ async function expand() {
 	windowLayer.classList.remove("opacity-0", "pointer-events-none");
 	const window = windowLayer.appendChild(document.createElement("div"));
 	window.id = "window";
-	new WindowContent({
-		target: window,
-	});
 	window.style.transitionTimingFunction = "cubic-bezier(0,.98,.58,.97)";
-	window.style.filter = "blur(10px)";
+	window.style.maxWidth = "96vw";
 	window.style.width = `${buttonW}px`;
 	window.style.height = `${buttonH}px`;
 	window.style.left = `${buttonX}px`;
@@ -58,25 +28,23 @@ async function expand() {
 		"border-black",
 		"fixed",
 		"duration-300",
-		"max-w-screen-lg",
 		"max-h-[96vh]",
 		"shadow-2xl",
 	);
-
 	await sleep(300);
 	window.style.width = "100vh";
 	window.style.height = "100vh";
 	window.style.left = "50%";
 	window.style.top = "50%";
-	window.style.filter = "blur(0px)";
 	window.style.transform = "translate(-50%, -50%)";
-	await sleep(1000);
-	close();
+	new WindowContent({
+		target: window,
+	});
 }
 </script>
 
 <div
-  class="window-layer pointer-events-none fixed inset-0 z-50 bg-white/10 opacity-0 duration-500"
+  class="window-layer pointer-events-none fixed inset-0 z-50 bg-white/80 opacity-0 duration-500"
 />
 <div class="flex justify-end p-2">
   <button
--- src/components/WindowContent.svelte
+++ src/components/WindowContent.svelte
@@ -1,4 +1,30 @@
 <script lang="ts">
-  
+import { sleep } from "../utils";
+async function close() {
+	const button = document.getElementById("projects")!;
+	const windowContent = document.getElementById("window-content")!;
+	windowContent.style.visibility = "hidden";
+	const {
+		width: buttonW,
+		height: buttonH,
+		x: buttonX,
+		y: buttonY,
+	} = button.getBoundingClientRect();
+	const window = document.getElementById("window")!;
+	const windowLayer = document.querySelector(".window-layer")!;
+	window.style.transform = "";
+	window.style.height = `${buttonH}px`;
+	window.style.width = `${buttonW}px`;
+	window.style.left = `${buttonX}px`;
+	window.style.top = `${buttonY}px`;
+	windowLayer.classList.add("opacity-0", "pointer-events-none");
+	await sleep(300);
+	window.remove();
+	button.style.visibility = "visible";
+}
 </script>
-<div>hello world</div>
+
+<div class="p-2 text-white" id="window-content">
+  <button on:click={() => close()}>Close</button>
+  <div>hello world</div>
+</div>

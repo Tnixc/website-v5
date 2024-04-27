69e580b6adcc5cc0e8ebb068d151beb8994ed694
Tnixc <tnixxc@gmail.com>
2024-04-26 23:29:23 -0400

    inline style?

--- svelte.config.js
+++ svelte.config.js
@@ -10,32 +10,36 @@ import remarkMath from "remark-math";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess(),
-		mdsvex({
-			extensions: [".md"],
-			layout: {
-				blog: "src/routes/blog/post.svelte",
-			},
-			remarkPlugins: [remarkHint],
-			rehypePlugins: [
-				rehypeKatexSvelte,
-				remarkMath,
-				rehypeSlug,
-				rehypeAutolinkHeadings,
-			],
-		}),
-	],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extensions: [".md"],
+      layout: {
+        blog: "src/routes/blog/post.svelte",
+      },
+      remarkPlugins: [remarkHint],
+      rehypePlugins: [
+        rehypeKatexSvelte,
+        remarkMath,
+        rehypeSlug,
+        rehypeAutolinkHeadings,
+      ],
+    }),
+  ],
 
-	extensions: [".svelte", ".md"],
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-	},
+  extensions: [".svelte", ".md"],
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    paths: {
+      relative: false,
+    },
+    inlineStyleThreshold: Infinity,
+    adapter: adapter(),
+  },
 };
 
 export default config;

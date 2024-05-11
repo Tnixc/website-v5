32ac982bb1e5c8f39296610300fa96dcc9514989
Tnixc <tnixxc@gmail.com>
2024-05-11 16:50:56 -0400

    add webring

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="unblur-in w-full bg-black/5 py-2 flex">
+<div class="unblur-in w-full bg-black/5 py-2 flex flex-wrap">
   <span class="px-2 opacity-50">/</span>
   <a class="hover:underline" href="https://github.com/Tnixc/website-v5"
     >Source code on Github</a
@@ -8,4 +8,13 @@
     class="hover:underline"
     href="https://us.umami.is/share/o4Mje48LZ8anrxZi/tnixc.space">Analytics</a
   >
+  <div class="flex mr-0 ml-auto gap-4 px-4">
+    <a class="hover:underline" href="https://ctp-webr.ing/tnixc/previous"
+      >&lt;-</a
+    ><a class="hover:underline" href="https://ctp-webr.ing/"
+      >Catppuccin webring</a
+    ><a class="hover:underline" href="https://ctp-webr.ing/tnixc/next">
+      -&gt;</a
+    >
+  </div>
 </div>
